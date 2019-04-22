932135ab5e724590c93345aea2f49fc817103b2a
yjh <yjh@163.com>
2019-04-23 07:38:17 +0800

    微信项目l

--- src/main/java/com/youzhong/controller/UserController.java
+++ src/main/java/com/youzhong/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -14,7 +15,7 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping("/test")
-
+     @ResponseBody
     public String test(Model model) {
         List list = userService.list();
         model.addAttribute("list", list);
