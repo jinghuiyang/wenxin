87726fba2dad30827e0d056e0fadfee693b1822e
yjh <yjh@163.com>
2019-04-22 19:34:52 +0800

    微信项目

---
+++ src/main/java/com/youzhong/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.youzhong.controller;
+
+import com.youzhong.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/test")
+
+    public String test(Model model) {
+        List list = userService.list();
+        model.addAttribute("list", list);
+        return "user";
+    }
+
+    @RequestMapping("/test1")
+    public String test1() {
+        return "user1";
+    }
+}
