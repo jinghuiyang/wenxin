3ffb8150acadb509d11aff33fd2ccce5752c397b
yjh <yjh@163.com>
2019-04-23 16:28:52 +0800

    微信项目l

--- src/main/java/com/youzhong/util/MenuManager.java
+++ src/main/java/com/youzhong/util/MenuManager.java
@@ -2,6 +2,7 @@ package com.youzhong.util;
 
 import com.youzhong.config.SchedledConfiguration;
 import com.youzhong.entity.AccessToken;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
